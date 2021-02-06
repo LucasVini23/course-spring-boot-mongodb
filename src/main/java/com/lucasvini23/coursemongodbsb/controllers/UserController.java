23e6b67dc6780571374ccb0a89c5c2586fef6793
Lucas <lucasvinipro23@gmail.com>
2021-02-06 15:50:10 -0300

    Obtendo um usuário por id

--- src/main/java/com/lucasvini23/coursemongodbsb/controllers/UserController.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/controllers/UserController.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucasvini23.coursemongodbsb.domain.User;
 import com.lucasvini23.coursemongodbsb.dto.UserDTO;
 import com.lucasvini23.coursemongodbsb.services.UserService;
 
@@ -22,5 +24,11 @@ public class UserController {
 	@GetMapping()
 	public ResponseEntity<List<UserDTO>> findAll(){
 		return ResponseEntity.ok().body(service.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList()));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/lucasvini23/coursemongodbsb/services/UserService.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucasvini23.coursemongodbsb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasvini23.coursemongodbsb.domain.User;
 import com.lucasvini23.coursemongodbsb.repository.UserRepository;
+import com.lucasvini23.coursemongodbsb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }
