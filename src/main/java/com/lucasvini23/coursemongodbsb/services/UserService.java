f9b6fe7fcc60e4f3c6dfa1886f27839fa3d5cc9b
Lucas <lucasvinipro23@gmail.com>
2021-02-07 19:22:20 -0300

    Inserção de usuário com POST

--- src/main/java/com/lucasvini23/coursemongodbsb/services/UserService.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucasvini23.coursemongodbsb.domain.User;
+import com.lucasvini23.coursemongodbsb.dto.UserDTO;
 import com.lucasvini23.coursemongodbsb.repository.UserRepository;
 import com.lucasvini23.coursemongodbsb.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
