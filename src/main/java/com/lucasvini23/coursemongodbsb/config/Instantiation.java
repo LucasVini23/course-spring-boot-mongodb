227dbf4cc6a9e537bf3c3d178e78b3b810b5f224
Lucas <lucasvinipro23@gmail.com>
2021-02-07 20:13:30 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/lucasvini23/coursemongodbsb/config/Instantiation.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.lucasvini23.coursemongodbsb.domain.Post;
 import com.lucasvini23.coursemongodbsb.domain.User;
 import com.lucasvini23.coursemongodbsb.dto.AuthorDTO;
+import com.lucasvini23.coursemongodbsb.dto.CommentDTO;
 import com.lucasvini23.coursemongodbsb.repository.PostRepository;
 import com.lucasvini23.coursemongodbsb.repository.UserRepository;
 
@@ -37,6 +38,13 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
