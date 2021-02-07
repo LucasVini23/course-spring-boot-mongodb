4ed8fc042ba6ad2d1aa8f23615e705c742f72999
Lucas <lucasvinipro23@gmail.com>
2021-02-07 20:25:22 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasvini23/coursemongodbsb/controllers/PostController.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/controllers/PostController.java
@@ -1,12 +1,16 @@
 package com.lucasvini23.coursemongodbsb.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucasvini23.coursemongodbsb.controllers.util.URL;
 import com.lucasvini23.coursemongodbsb.domain.Post;
 import com.lucasvini23.coursemongodbsb.services.PostService;
 
@@ -21,6 +25,13 @@ public class PostController {
 	public ResponseEntity<Post> findById(@PathVariable String id){
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
+	}
+	
+	@GetMapping("/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
