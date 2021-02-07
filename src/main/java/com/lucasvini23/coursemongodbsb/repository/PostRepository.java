99612206d20ff75f0e12edd1297ef5cf675955f1
Lucas <lucasvinipro23@gmail.com>
2021-02-07 20:30:31 -0300

    Consulta simples com @Query

--- src/main/java/com/lucasvini23/coursemongodbsb/repository/PostRepository.java
+++ src/main/java/com/lucasvini23/coursemongodbsb/repository/PostRepository.java
@@ -3,11 +3,15 @@ package com.lucasvini23.coursemongodbsb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.lucasvini23.coursemongodbsb.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
